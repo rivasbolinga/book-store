0073be7c88f4756545e7a0774357e43674e898f4
Andrea <andrearivas4cinfor@gmail.com>
2023-03-01 13:59:18 +0100

    Add booklist file

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,12 +1,6 @@
 import Book from './Book';
 import Form from './Form';
 
-const bookList = [
-  { id: 0, title: 'The alchemist', author: 'Paulo Coelho' },
-  { id: 1, title: 'A fine balance', author: 'Rohinton Mistry' },
-  { id: 2, title: '1984', author: 'George Orwell' },
-];
-
 const Books = () => (
   <>
     <section className="book-section">
